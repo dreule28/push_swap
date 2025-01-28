b2e7d5a89fac764c3647e913e5f2d1a1f323ce45
Daniel Reule <dreule@3-B-12.42heilbronn.de>
2025-01-28 09:58:06 +0100

    CHANGE(rra/rrb/rrr): cleaned up code and added write commands after execution

--- src/list/op-->rra&rrb&rrr.c
+++ src/list/op-->rra&rrb&rrr.c
@@ -6,48 +6,44 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 20:41:31 by dreule            #+#    #+#             */
-/*   Updated: 2025/01/24 21:04:59 by dreule           ###   ########.fr       */
+/*   Updated: 2025/01/28 09:57:46 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rra(t_list *stack_a)
+void	rev_rotate(t_list *stack)
 {
 	t_node	*first;
 	t_node	*last;
 
-	if (stack_a->head == NULL || stack_a->tail == NULL)
+	if (stack->head == NULL || stack->tail == NULL)
 		return ;
-	first = stack_a->head;
-	last = stack_a->tail;
-	stack_a->tail = last->prev;
-	stack_a->tail->next = NULL;
+	first = stack->head;
+	last = stack->tail;
+	stack->tail = last->prev;
+	stack->tail->next = NULL;
 	last->prev = NULL;
 	last->next = first;
 	first->prev = last;
-	stack_a->head = last;
+	stack->head = last;
+}
+
+void	rra(t_list *stack_a)
+{
+	rev_rotate(stack_a);
+	write(1, "rra\n", 4);
 }
 
 void	rrb(t_list *stack_b)
 {
-	t_node	*first;
-	t_node	*last;
-
-	if (stack_b->head == NULL || stack_b->tail == NULL)
-		return ;
-	first = stack_b->head;
-	last = stack_b->tail;
-	stack_b->tail = last->prev;
-	stack_b->tail->next = NULL;
-	last->prev = NULL;
-	last->next = first;
-	first->prev = last;
-	stack_b->head = last;
+	rev_rotate(stack_b);
+	write(1, "rrb\n", 4);
 }
 
 void	rrr(t_list *stack_a, t_list *stack_b)
 {
-	rra(stack_a);
-	rrb(stack_b);
+	rev_rotate(stack_a);
+	rev_rotate(stack_b);
+	write(1, "rrr\n", 4);
 }
