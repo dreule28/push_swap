ff87faca03c03b07f3298e1e9a7c331aea942f2e
Daniel Reule <dreule@3-B-12.42heilbronn.de>
2025-01-28 10:01:16 +0100

    CHANGE(sa/sb/ss): cleaned up code and added write commands after execution

--- src/list/op-->sa&sb&ss.c
+++ src/list/op-->sa&sb&ss.c
@@ -6,52 +6,45 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 10:46:03 by dreule            #+#    #+#             */
-/*   Updated: 2025/01/24 20:31:49 by dreule           ###   ########.fr       */
+/*   Updated: 2025/01/28 10:00:49 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_list *stack_a)
+void	swap(t_list *stack)
 {
 	t_node	*first;
 	t_node	*second;
 
-	if (!stack_a || !stack_a->head || !stack_a->head->next)
+	if (!stack || !stack->head || !stack->head->next)
 		return ;
-	first = stack_a->head;
-	second = stack_a->head->next;
+	first = stack->head;
+	second = stack->head->next;
 	first->next = second->next;
 	if (second->next != NULL)
 		second->next->prev = first;
 	second->prev = NULL;
 	second->next = first;
 	first->prev = second;
-	stack_a->head = second;
-	write(1, "sa", 1);
+	stack->head = second;
+}
+
+void	sa(t_list *stack_a)
+{
+	swap(stack_a);
+	write(1, "sa\n", 3);
 }
 
 void	sb(t_list *stack_b)
 {
-	t_node	*first;
-	t_node	*second;
-
-	if (!stack_b || !stack_b->head || !stack_b->head->next)
-		return ;
-	first = stack_b->head;
-	second = stack_b->head->next;
-	first->next = second->next;
-	if (second->next != NULL)
-		second->next->prev = first;
-	second->prev = NULL;
-	second->next = first;
-	first->prev = second;
-	stack_b->head = second;
-	write(1, "sb", 1);
+	swap(stack_b);
+	write(1, "sb\n", 3);
 }
 
 void	ss(t_list *stack_a, t_list *stack_b)
 {
-	sa(stack_a);
-	sb(stack_a);
+	swap(stack_a);
+	swap(stack_b);
+	write(1, "ss\n", 3);
 }
