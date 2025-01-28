5444020f496b0e189c9c894c14d33ca222dfee72
Daniel Reule <dreule@3-B-12.42heilbronn.de>
2025-01-28 10:04:39 +0100

    ADD(list.h): added prototypes to header

--- Includes/list.h
+++ Includes/list.h
@@ -7,14 +7,17 @@
 # include <stdio.h>
 # include <unistd.h>
 //Includes -- END
+void	swap(t_list *stack);
 void	sa(t_list *stack_a);
 void	sb(t_list *stack_b);
 void	ss(t_list *stack_a, t_list *stack_b);
 void	pa(t_list *stack_a, t_list *stack_b);
 void	pb(t_list *stack_a, t_list *stack_b);
+void	rotate(t_list *stack);
 void	ra(t_list *stack_a);
 void	rb(t_list *stack_b);
 void	rr(t_list *stack_a, t_list *stack_b);
+void	rev_rotate(t_list *stack);
 void	rra(t_list *stack_a);
 void	rrb(t_list *stack_b);
 void	rrr(t_list *stack_a, t_list *stack_b);
