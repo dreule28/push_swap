15040c60b4c4a27d12a48c96651e9c226ee5c6cc
Daniel Reule <dreule@2-I-1.42heilbronn.de>
2025-01-31 18:06:44 +0100

    RENAME(pa...): renamed pa&pb&pp to push

---
+++ src/list/push.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 20:31:35 by dreule            #+#    #+#             */
+/*   Updated: 2025/01/31 18:02:04 by dreule           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	pa(t_list *stack_a, t_list *stack_b)
+{
+	t_node	*temp;
+
+	if (stack_b == NULL || stack_b->head == NULL)
+		return ;
+	temp = stack_b->head;
+	stack_b->head = stack_b->head->next;
+	if (stack_b->head)
+		stack_b->head->prev = NULL;
+	else
+		stack_b->tail = NULL;
+	temp->next = stack_a->head;
+	if (stack_a->head)
+		stack_a->head->prev = temp;
+	else
+		stack_a->tail = temp;
+	stack_a->head = temp;
+	stack_a->size++;
+	stack_b->size--;
+	write(1, "pa\n", 3);
+}
+
+void	pb(t_list *stack_a, t_list *stack_b)
+{
+	t_node	*temp;
+
+	if (stack_a == NULL || stack_a->head == NULL)
+		return ;
+	temp = stack_a->head->next;
+	stack_a->head = stack_a->head->next;
+	if (stack_a->head)
+		stack_a->head->prev = NULL;
+	else
+		stack_a->tail = temp;
+	temp->next = stack_b->head;
+	if (stack_b->head)
+		stack_b->head->prev = temp;
+	else
+		stack_b->tail = temp;
+	stack_b->head = temp;
+	stack_a->size--;
+	stack_b->size++;
+	write(1, "pa\n", 3);
+}
