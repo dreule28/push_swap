1246347ae2f6f0faa87c8c51a47c0093ad3b4855
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-03 11:19:30 +0100

    ADD(parser.c): added build_up_args function

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,7 +1,37 @@
-#include "push_swap.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 10:39:26 by dreule            #+#    #+#             */
+/*   Updated: 2025/02/03 11:17:52 by dreule           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "parser.h"
 
-void	print_msg(void)
+char	*build_up_args(char **argv)
 {
-	write (1, "In Parser\n", ft_strlen("In Parser\n"));
+	char	*joined;
+	int		i;
+	int		len;
+
+	i = 1;
+	len = 0;
+	while (argv[i])
+		len += ft_strlen(argv[i++]) + 1;
+	joined = malloc(len + 1);
+	if (!joined)
+		return (NULL);
+	joined[0] = '\0';
+	i = 1;
+	while (argv[i])
+	{
+		ft_strlcat(joined, argv[i], len);
+		ft_strlcat(joined, " ", len);
+		i++;
+	}
+	return (joined);
 }
