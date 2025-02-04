309d90c36803c23d1bfe6478855898d2b3971a41
Daniel Reule <dreule@2-G-1.42heilbronn.de>
2025-02-04 16:02:36 +0100

    FIX(fill_stack.c): moved free into while loop to prevent leaks

--- src/parser/fill_stack.c
+++ src/parser/fill_stack.c
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:25:24 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/04 14:44:44 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/04 15:56:10 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void	fill_stack(t_list *stack_a, int *real_ints)
 		if (!new_node)
 			free_stack(stack_a);
 		add_end(stack_a, new_node);
+		free(new_node);
 		i++;
 	}
-	free(new_node);
 }
