a3a7060c39aac34b57f41d95d91e513205cbf3c4
Daniel Reule <dreule@2-G-1.42heilbronn.de>
2025-02-04 10:28:31 +0100

    ADD(parser.c): added failchecks, added check_dupes function

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,28 +6,72 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 10:39:26 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/03 13:45:29 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/04 10:20:01 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
+int	*check_dupes(char *args, t_list *stack_a)
+{
+	char	**nbs;
+	int		*ints;
+	int		len;
 
+	len = 0;
+	nbs = ft_split(args, ' ');
+	if (!nbs)
+		return ((int *)NULL);
+	while (nbs[len])
+		len++;
+	ints = malloc(sizeof(int) * len);
+	if (!ints)
+		return ((int *)NULL);
+	len = 0;
+	while (nbs[len])
+	{
+		ints[len] = ft_atoi(nbs[len]);
+		free(nbs[len]);
+		len++;
+	}
+	free(nbs);
+	if (is_dup(ints, len))
+		free_ints(args, ints);
+	stack_a->size = len;
+	return (ints);
+}
 
-bool	is_valid(char *ints)
+bool	is_dup(int *ints, int len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len)
+	{
+		j = i + 1;
+		while (j < len)
+			if (ints[i] == ints[j++])
+				return (true);
+		i++;
+	}
+	return (false);
+}
+
+bool	is_valid(char *str)
 {
 	int		i;
 	long	nb;
 
 	i = 0;
-	nb = ft_atol(ints);
-	if (ints[i] == "-" || ints[i] == "+")
+	nb = ft_atol(str);
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-	if (!ints[i])
+	if (!str[i])
 		return (false);
-	while (ints[i++])
+	while (str[i++])
 	{
-		if (!(ints[i] >= 0 && ints[i] <= 9))
+		if (!(str[i] >= 0 && str[i] <= 9))
 			return (false);
 	}
 	if (nb > INT_MAX || nb < INT_MIN)
@@ -59,24 +103,26 @@ char	*build_up_args(char **argv)
 	return (joined);
 }
 
-void	pars_args(char **argv)
+void	pars_args(char **argv, t_list *stack_a)
 {
-	int	i;
-	int	**ints;
-	int	*joined_args;
+	int			i;
+	char		**ints;
+	char		*joined_args;
+	int			*real_ints;
 
 	i = 0;
 	joined_args = build_up_args(argv);
-	if (!joined_args)
+	if (!joined_args || !*joined_args)
 		free_joined_args(joined_args);
 	ints = ft_split(joined_args, ' ');
 	if (!ints)
 		free_joined_args(joined_args);
 	while (ints[i])
-	{
 		if (!is_valid(ints[i++]))
 			free_all(joined_args, ints);
-		free_2d(ints);
-	}
-
+	free_2d(ints);
+	real_ints = check_dupes(joined_args, stack_a);
+	if (!real_ints)
+		free_joined_args(joined_args);
+	
 }
