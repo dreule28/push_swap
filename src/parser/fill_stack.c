5a570468f20a98dc64a649046a4b3d257f21d6c4
Daniel Reule <dreule@2-G-1.42heilbronn.de>
2025-02-04 14:38:00 +0100

    FIX(): added free to fix at end of fill_stack to fix leaks

--- src/parser/fill_stack.c
+++ src/parser/fill_stack.c
@@ -6,11 +6,12 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:25:24 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/04 10:19:51 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/04 14:32:15 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+#include <stdio.h>
 
 t_node	*add_node(int *value)
 {
@@ -18,10 +19,11 @@ t_node	*add_node(int *value)
 
 	new_node = malloc(sizeof(t_node));
 	if (!new_node)
-		return (t_node *)(NULL);
+		return ((t_node *) (NULL));
 	new_node->next = NULL;
 	new_node->prev = NULL;
-	new_node->value = value;
+	new_node->value = (*value);
+	printf("Value of node : %d\n", new_node->value);
 	return (new_node);
 }
 
@@ -43,16 +45,17 @@ void	add_end(t_list *stack, t_node *new_node)
 
 void	fill_stack(t_list *stack_a, int *real_ints)
 {
-	t_node	*new_node;
-	int		i;
+	t_node		*new_node;
+	size_t		i;
 
 	i = 0;
 	while (i < stack_a->size)
 	{
-		new_node = add_node(real_ints);
+		new_node = add_node(&real_ints[i]);
 		if (!new_node)
 			free_stack(stack_a);
 		add_end(stack_a, new_node);
 		i++;
 	}
+	free(new_node);
 }
