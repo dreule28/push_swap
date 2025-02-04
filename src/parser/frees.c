be09ef6cfe7c55cb823e64acd45e4e3285349a1c
Daniel Reule <dreule@2-G-1.42heilbronn.de>
2025-02-04 10:26:34 +0100

    ADD(frees.c): added free_stack func

--- src/parser/frees.c
+++ src/parser/frees.c
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:57:39 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/03 15:58:05 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/04 10:24:34 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,26 @@ void	free_all(char *joined, char **ints)
 	error("Error\n");
 }
 
-void	free_ints(int *ints)
-{
-	int	i;
-
-	i = 0;
-	while (ints[i])
-		free(ints[i++]);
-	free(ints);
-}
-
 void	free_ints(char *str, int *ints)
 {
 	free(str);
 	free(ints);
+	error("Error\n");
+}
+
+void	free_stack(t_list *stack)
+{
+	t_node	*curr;
+
+	curr = stack->head;
+	while(curr->next)
+	{
+		curr = curr->next;
+		free(curr->prev);
+	}
+	free(curr);
+	stack->head = NULL;
+	stack->tail = NULL;
+	stack->size = 0;
 	error("Error\n");
 }
