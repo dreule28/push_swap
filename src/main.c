1083c419f09891582f9df25d11eab30914459610
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-05 18:42:23 +0100

    RUN(main.c): program running for 2&3 nodes

--- src/main.c
+++ src/main.c
@@ -6,22 +6,26 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 19:32:02 by dreule            #+#    #+#             */
-/*   Updated: 2024/12/27 20:36:55 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/05 13:30:21 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	printf("Post ft_atoi: %d\n", ft_atoi("12345"));
-	printf("Tets pushswap\n");
+	t_list	stack_a;
+	t_list	stack_b;
 
-
-	print_msg();
-	print_msgALGO();
+	if (argc < 2)
+		error("Error\n");
+	init_stacks(&stack_a, &stack_b);
+	pars_args(argv, &stack_a);
+	sort_vals(&stack_a, &stack_b);
+	free_stack(&stack_a);
 	return (0);
 }
+/*
+	dorker valgrind --leak-check=full
+*/
