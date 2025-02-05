7677b3e8baac78d000c3c46fed898f295b904881
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-05 18:44:09 +0100

    ADD(algo.h): added libs and prototypes

--- Includes/algo.h
+++ Includes/algo.h
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:05:29 by dreule            #+#    #+#             */
-/*   Updated: 2025/01/31 11:06:06 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/05 18:29:15 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 
 //Includes -- BEGIN
 # include "libft.h"
+# include "list.h"
+# include "parser.h"
 # include <stdlib.h>
 # include <stdio.h>
+# include <stdbool.h>
 # include <unistd.h>
 //Includes -- END
 
 //FUNCTIONS -- BEGIN
+void	sort_vals(t_list *stack_a, t_list *stack_b);
+bool	is_sorted(t_list *stack_a);
+void	sort_three_nodes(t_list *stack_a);
 //FUNCTIONS -- END
 
 #endif
