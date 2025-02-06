12a0cee78e553b5aa605e11e76aebdbc5d837cb2
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-06 16:53:38 +0100

    MOVE(sort_stack.c): moved sorting functions to seperate file

---
+++ src/algo/sort_stack.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_stack.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 16:52:00 by dreule            #+#    #+#             */
+/*   Updated: 2025/02/06 16:52:49 by dreule           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "algo.h"
+
+void	sort_three_nodes(t_list *stack_a)
+{
+	t_node	*biggest_node;
+
+	biggest_node = stack_a->head;
+	if (stack_a->head->next->value > biggest_node->value)
+		biggest_node = stack_a->head->next;
+	if (stack_a->tail->value > biggest_node->value)
+		biggest_node = stack_a->tail;
+	if (biggest_node == stack_a->head)
+	{
+		ra(stack_a);
+		if (!is_sorted(stack_a))
+			sa(stack_a);
+	}
+	else if (biggest_node == stack_a->head->next)
+	{
+		rra(stack_a);
+		if (!is_sorted(stack_a))
+			sa(stack_a);
+	}
+	else if (!is_sorted(stack_a))
+		sa(stack_a);
+}
+
+void	sort_all(t_list *stack_a)
+{
+	get_index(stack_a);
+}
