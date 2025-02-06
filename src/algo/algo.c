dd0115564bc9a774cd26425cd3e6608b922e7cd7
Daniel Reule <dreule@3-B-12.42heilbronn.de>
2025-02-06 15:44:53 +0100

    ADD(algo.c): implemented get_index function

--- src/algo/algo.c
+++ src/algo/algo.c
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:45:11 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/05 18:34:54 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/06 15:44:01 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,14 +57,44 @@ void	sort_three_nodes(t_list *stack_a)
 		sa(stack_a);
 }
 
+void	get_index(t_list *stack_a)
+{
+	t_node	*curr;
+	t_node	*compare;
+
+
+	curr = stack_a->head;
+	while (curr)
+	{
+		curr->index = 1;
+		compare = stack_a->head;
+		while (compare)
+		{
+			if (compare->value < curr->value)
+				curr->index++;
+			compare = compare->next;
+		}
+		curr = curr->next;
+	}
+}
+
+void	sort_all(t_list *stack_a)
+{
+	get_index(stack_a);
+}
+
 void	sort_vals(t_list *stack_a, t_list *stack_b)
 {
 	(void)stack_b;
 	printf("Pre-Swap\nValue1: %d\nValue2: %d\nValue3: %d\n", stack_a->head->value, stack_a->head->next->value, stack_a->tail->value);
+	printf("Pre-Sort\nIndex1: %d\nIndex2: %d\nIndex3: %d\n", stack_a->head->index, stack_a->head->next->index, stack_a->tail->index);
 	if (stack_a->size == 2 && !is_sorted(stack_a))
 		sa(stack_a);
 	// printf("Post-Swap(2)\nValue1: %d\nValue2: %d\n", stack_a->head->value, stack_a->head->next->value);
 	if (stack_a->size == 3 && !is_sorted(stack_a))
 		sort_three_nodes(stack_a);
-	printf("Post-Swap(3)\nValue1: %d\nValue2: %d\nValue2: %d\n", stack_a->head->value, stack_a->head->next->value, stack_a->tail->value);
+	printf("Post-Swap(3)\nValue1: %d\nValue2: %d\nValue2: %d\n\n", stack_a->head->value, stack_a->head->next->value, stack_a->tail->value);
+	printf("Post-Sort\nIndex1: %d\nIndex2: %d\nIndex3: %d\n", stack_a->head->index, stack_a->head->next->index, stack_a->tail->index);
+	if (stack_a->size > 3 && !is_sorted(stack_a))
+		sort_all(stack_a);
 }
