e89fe6c868ade0fd76265025f75ae417894283e0
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-07 22:41:11 +0100

    ADD(prototypes): added new prototypes

--- Includes/algo.h
+++ Includes/algo.h
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:05:29 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/05 18:29:15 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/07 22:38:32 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,12 @@
 void	sort_vals(t_list *stack_a, t_list *stack_b);
 bool	is_sorted(t_list *stack_a);
 void	sort_three_nodes(t_list *stack_a);
+void	sort_all(t_list *stack_a, t_list *stack_b);
+void	get_index(t_list *stack_a);
+void	get_pos(t_list *stack_a, t_list *stack_b);
+void	split_to_three(t_list *stack_a, t_list *stack_b);
+void	find_targets(t_list *stack_a, t_list *stack_b);
+t_node	*get_smallest_index(t_list *stack_a);
 //FUNCTIONS -- END
 
 #endif
