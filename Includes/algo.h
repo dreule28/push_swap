7bf197aed92f703e5edfcaba603bff576ab158df
Daniel Reule <dreule@2-I-7.42heilbronn.de>
2025-02-10 12:28:49 +0100

    ADD(algo.h): added final prototypes

--- Includes/algo.h
+++ Includes/algo.h
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:05:29 by dreule            #+#    #+#             */
-/*   Updated: 2025/02/07 22:38:32 by dreule           ###   ########.fr       */
+/*   Updated: 2025/02/09 01:44:57 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,13 @@ void	get_index(t_list *stack_a);
 void	get_pos(t_list *stack_a, t_list *stack_b);
 void	split_to_three(t_list *stack_a, t_list *stack_b);
 void	find_targets(t_list *stack_a, t_list *stack_b);
-t_node	*get_smallest_index(t_list *stack_a);
+t_node	*get_smallest_index(t_node *head_a);
+void	assign_costs(t_list *stack_a, t_list *stack_b);
+void	find_cheapest_move(t_list *stack_a, t_list *stack_b);
+void	chose_ops(t_list *stack_a, t_list *stack_b);
+void	do_rrr(t_list *stack_a, t_list *stack_b);
+void	do_rr(t_list *stack_a, t_list *stack_b);
+void	rot_or_revrot(t_list *stack_a, t_list *stack_b);
 //FUNCTIONS -- END
 
 #endif
